5952f344d64275162a049cf26c4892ec8a167af9
Yuki Kodama <endflow.net@gmail.com>
2020-11-10 00:17:20 +0900

    First ray

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # minitracer
+
 [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) written in Rust
 
 ## Output
@@ -6,3 +7,7 @@
 ### Chapter 1
 
 ![c1](https://raw.githubusercontent.com/kuy/minitracer/main/output/ch1.png?raw=true)
+
+### Chapter 3
+
+![c1](https://raw.githubusercontent.com/kuy/minitracer/main/output/ch3.png?raw=true)
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate approx;
 mod ray;
 mod vec3;
 
+use ray::Ray;
 use vec3::Vec3;
 
 fn main() {
@@ -11,13 +12,21 @@ fn main() {
     let ny = 100;
     println!("P3\n{} {}\n255", nx, ny);
 
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+
     for j in 0..ny {
         for i in 0..nx {
-            let col = Vec3::new(
-                f64::from(i) / f64::from(nx),
-                f64::from(ny - j) / f64::from(ny),
-                0.2,
+            let u = f64::from(i) / f64::from(nx);
+            let v = f64::from(ny - j) / f64::from(ny);
+
+            let r = Ray::new(
+                origin.clone(),
+                lower_left_corner.clone() + u * horizontal.clone() + v * vertical.clone(),
             );
+            let col = color(r);
 
             let ir = (255.0 * col.r()).round() as i32;
             let ig = (255.0 * col.g()).round() as i32;
@@ -26,4 +35,10 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+}
+
+fn color(r: Ray) -> Vec3 {
+    let unit_dir = r.direction().to_unit();
+    let t = (unit_dir.y() + 1.0) / 2.0;
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
