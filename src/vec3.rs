5341f3f0f63006c238f767e092287fdd14d4ad55
Yuki Kodama <endflow.net@gmail.com>
2020-11-09 21:52:32 +0900

    Impl. add overload

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 #[derive(Debug)]
 pub struct Vec3 {
     e: (f64, f64, f64),
@@ -48,15 +50,36 @@ impl Vec3 {
 
 impl PartialEq for Vec3 {
     fn eq(&self, other: &Self) -> bool {
-        abs_diff_eq!(self.x(), other.x(), epsilon = 1e-3f64)
-            && abs_diff_eq!(self.y(), other.y(), epsilon = 1e-3f64)
-            && abs_diff_eq!(self.z(), other.z(), epsilon = 1e-3f64)
+        abs_diff_eq!(self.e.0, other.e.0, epsilon = 1e-3f64)
+            && abs_diff_eq!(self.e.1, other.e.1, epsilon = 1e-3f64)
+            && abs_diff_eq!(self.e.2, other.e.2, epsilon = 1e-3f64)
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            e: (
+                self.e.0 + other.e.0,
+                self.e.1 + other.e.1,
+                self.e.2 + other.e.2,
+            ),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_to_add() {
+        let v1 = Vec3::new(3.0, 4.0, 5.0);
+        let v2 = Vec3::new(2.2, 0.7, 0.25);
+        assert_eq!(v1 + v2, Vec3::new(5.2, 4.7, 5.25));
+    }
 
     #[test]
     fn test_length() {
