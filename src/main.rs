bd86fed768ab38c28025f6199bae61c5685f1098
Yuki Kodama <endflow.net@gmail.com>
2020-11-09 23:03:11 +0900

    Use Vec3

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate approx;
 
 mod vec3;
 
+use vec3::Vec3;
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -10,13 +12,15 @@ fn main() {
 
     for j in 0..ny {
         for i in 0..nx {
-            let r = f64::from(i) / f64::from(nx);
-            let g = f64::from(ny - j) / f64::from(ny);
-            let b = 0.2f64;
+            let col = Vec3::new(
+                f64::from(i) / f64::from(nx),
+                f64::from(ny - j) / f64::from(ny),
+                0.2,
+            );
 
-            let ir = (256.0 * r).round() as i32;
-            let ig = (256.0 * g).round() as i32;
-            let ib = (256.0 * b).round() as i32;
+            let ir = (255.0 * col.r()).round() as i32;
+            let ig = (255.0 * col.g()).round() as i32;
+            let ib = (255.0 * col.b()).round() as i32;
 
             println!("{} {} {}", ir, ig, ib);
         }
