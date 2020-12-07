2fef48686813f17d417a8992b09cfec048ccec68
Yuki Kodama <endflow.net@gmail.com>
2020-12-07 22:51:44 +0900

    Refactoring: catch up latest book

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,31 @@ use sphere::Sphere;
 use vec3::{Point3, Vec3};
 
 fn main() {
-    let nx = 200;
-    let ny = 100;
-    println!("P3\n{} {}\n255", nx, ny);
+    // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (f64::from(image_width) / aspect_ratio) as i32;
 
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
 
-    for j in 0..ny {
-        for i in 0..nx {
-            let u = f64::from(i) / f64::from(nx);
-            let v = f64::from(ny - j) / f64::from(ny);
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin.clone()
+        - (horizontal.clone() * 0.5)
+        - (vertical.clone() * 0.5)
+        - Vec3::new(0.0, 0.0, focal_length);
+
+    // Render
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let u = f64::from(i) / f64::from(image_width - 1);
+            let v = f64::from(image_height - j) / f64::from(image_height - 1);
 
             let r = Ray::new(
                 origin.clone(),
